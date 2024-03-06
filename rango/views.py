b15864d2a5a0bdc6961d28365ef186f0eb8845c4
“Aiden <2747612l@student.gla.ac.uk>
2024-03-06 17:54:49 +0000

    added the authentication models

--- rango/views.py
+++ rango/views.py
@@ -1,5 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.forms import UserForm,UserProfileForm
 # Create your views here.
 def index(request):
     return render(request, 'rango/index.html')
+
+def register(request):
+    registered = False
+
+    if request.method == 'POST':
+        user_form = UserForm(request.POST)
+        profile_form = UserProfileForm(request.POST)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+
+            user.set_password(user.password)
+            user.save()
+
+            profile = profile_form.save(commit=False)
+            profile.user = user
+
+            if 'picture' in request.FILES:
+                profile.picture = request.FILES['picture']
+
+            profile.save()
+
+            registered = True
+
