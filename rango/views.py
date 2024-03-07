8ede507d5be993a563878073cccf6004a6110974
AaronMcintyre255 <aaronlmcintyre255@gmail.com>
2024-03-07 20:53:09 +0000

    Profile View Skeleton Updated

--- rango/views.py
+++ rango/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from rango.models import Book
+from rango.models import Category
 
 def index(request):
     return render(request, 'rango/index.html')
 
 
 def profile(request):
-    return render(request, 'rango/profile.html')
+    
+    category_list = Category.objects.order_by('-activity')[:5]
+    book_list = Book.objects.order_by('-readingOrder')[:5]
+
+    context_dict = {}
+    context_dict['categories'] = category_list
+    context_dict['book'] = book_list
+
+    return render(request, 'rango/profile.html', context=context_dict)
