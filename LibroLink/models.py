25a49f6fbacfee10e8665e6c7c58773de9acaca0
“Aiden <87037572+aidenlindsay@users.noreply.github.com>
2024-03-12 17:26:17 +0000

    Added a category model and cahnged profile to build from base

--- LibroLink/models.py
+++ LibroLink/models.py
@@ -1,7 +1,28 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.template.defaultfilters import slugify
 
 # Create your models here.
+
+class Category(models.Model):
+
+    NAME_MAX_LENGTH = 128
+
+    name = models.CharField(max_length=NAME_MAX_LENGTH, unique=True)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+    slug = models.SlugField(unique=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
+
+    class Meta:
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
+
 class UserProfile(models.Model): 
     user = models.OneToOneField(User, on_delete=models.CASCADE) 
     website = models.URLField(blank=True) 
