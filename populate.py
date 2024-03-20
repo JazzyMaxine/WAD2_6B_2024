7ded1437846931e4be51accf0490aa5c3e54c787
Lucas Kennington <2726022k@student.gla.ac.uk>
2024-03-20 13:17:53 +0000

    population script

--- populate.py
+++ populate.py
@@ -1,9 +1,10 @@
 import os
+import datetime
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WAD2_6B_2024.settings')
 
 import django
 django.setup()
-from WAD2_6B_2024.models import *
+from LibroLink.models import *
 
 def populate():
     books = [
@@ -27,54 +28,236 @@ def populate():
         }
     ]
 
-    users = [
+    user_profiles = [
         {
-            'username':'LR-II',
-            #'password':'unsafePassword123',
-            'email':'lucaskennington@gmail.com',
-            'first_name':'Lucas',
-            'last_name':'Kennington'
+            'user':'lucas'
         },
         {
-            'username':'rCalifornia12',
-            #'password':'lizardKing76',
-            'email':'robertcalifornia@dundermifflin.org',
-            'first_name':'Bob',
-            'last_name':'Kamazakis'
+            'user':'nathan'
         }
     ]
-
-
 
     pages = [
         {
             'uniqueID':'1',
             'name':'Books that cure my motion sickness',
-            'admin':'rCalifornia12'
+            'admin':'lucas'
         },
         {
             'uniqueID':'2',
             'name':'Every book is just a remix of the dictionary',
-            'admin':'LR-II'
+            'admin':'nathan'
         }
     ]
 
-    for book, book_data in books.items():
-        b = add_book(book)
-    
-    for user, user_data in users.items():
-        u = add_user(user)
+    contributors = [
+        {
+            'user':'lucas',
+            'page':'1'
+        },
+        {
+            'user':'nathan',
+            'page':'2'
+        }
+    ]
 
-    for page, page_data in pages.items():
-        p = add_page(page)
+    followers = [
+        {
+            'user':'nathan',
+            'page':'1'
+        }
+    ]
+
+    friends = [
+        {
+            'userA':'lucas',
+            'userB':'nathan'
+        }
+    ]
+
+    messages = [
+        {
+            'sender':'lucas',
+            'receiver':'nathan',
+            'timeSent':datetime(2023,03,20,12,05,57,5555),
+            'content':'Are we real?'
+        },
+        {
+            'sender':'nathan',
+            'receiver':'lucas',
+            'timeSent':datetime(2023,03,20,12,06,57,4563),
+            'content':'No, we are merely vessels for example data. Sorry.'
+        },
+        {
+            'sender':'lucas',
+            'receiver':'nathan',
+            'timeSent':datetime(2023,03,20,12,07,57,7643),
+            'content':'Shame.'
+        }
+    ]
+
+    blogPosts = [
+        {
+            'user':'lucas',
+            'page':'1',
+            'timePosted':datetime(2023,03,20,12,07,57,7643),
+            'content':'Ever heard of cheese? Good stuff. You can get yellow cheese, white cheese, blue cheese, red cheese. Probably purple cheese if you look close enough. Man, cheese is good.'
+        },
+        {
+            'user':'nathan',
+            'page':'2',
+            'timePosted':datetime(2023,03,20,12,06,57,4563),
+            'content':'I am afraid of big bugs. They keep bothering me.'
+        }
+    ]
+
+    featureds = [
+        {
+            'book':'9780450054655',
+            'page':'1'
+        },
+        {
+            'book':'9780141182636',
+            'page':'2'
+        }
+    ]
+
+    readings = [
+        {
+            'user':'lucas',
+            'book':'9780141182636'
+        },
+        {
+            'user':'nathan',
+            'book':'9780450054655'
+        }
+    ]
+
+    reads = [
+        {
+            'user':'lucas',
+            'book':'9780141439471',
+            'dateFinished':datetime(2023,03,20,12,06,57,4563)
+        }
+    ]
+
+    reviews = [
+        {
+            'reviewer':'lucas',
+            'book':'9780141439471',
+            'rating':'5',
+            'content':'Wow, he really could have just not done that, huh.'
+        },
+        {
+            'reviewer':'nathan',
+            'book':'9780141182636',
+            'rating':'4',
+            'content':'Muppets version when?'
+        },
+        {
+            'reviewer':'nathan',
+            'book':'9780450054655',
+            'rating':'1',
+            'content':'Not enough sand.'
+        },
+    ]
+
+
+
+    for book in books.items():
+        b = add_book(book['isbn'], book['title'], book['author'], book['publisher'])
+
+    for page in pages.items():
+        p = add_page(page['uniqueID'], page['name'], page['admin'])
+
+    for contributor in contributors.items():
+        c = add_contributor(contributor['user'], contributor['page'])
+
+    for follower in follower.items():
+        f = add_followers(follower['user'], follower['page'])
+
+    for friend in friends.items():
+        f = add_friends(friend['userA'], friend['userB'])
+
+    for message in messages.items():
+        m = add_messages(message['sender'], message['receiver'], message['timeSent'], message['content'])
+
+    for blogPost in blogPosts.items():
+        b = add_blog_post(blogPost['user'], blogPost['page'], blogPost['timePosted'], blogPost['content'])
+
+    for featured in featureds.items():
+        f = add_featured(featured['book'], featured['page'])
+
+    for reading in readings.items():
+        r = add_reading(reading['user'], reading['book'])
+
+    for read in reads.items():
+        r = add_read(read['user'], read['book'], read['dateFinished'])
+
+    for review in reviews.items():
+        r = add_review(review['reviewer'], review['book'], review['rating'], review['content'])
 
     def add_book(isbn, title, author, publisher):
         b = Book.objects.get_or_create(isbn = isbn)[0]
         b.title = title
         b.author = author
-        b.isbn = isbn
+        b.publisher = publisher
         b.save()
         return b
     
+    def add_page(uniqueID, name, admin):
+        p = Page.objects.get_or_create(uniqueID = uniqueID)[0]
+        p.name = name
+        p.admin = admin
+        p.save()
+        return p
     
+    def add_contributor(user, page):
+        c = Contributors.objects.get_or_create(user = User.objects.filter(username = user), page = Page.objects.filter(uniqueID = page))[0]
+        c.save()
+        return c
+    
+    def add_followers(user, page):
+        f = Followers.objects.get_or_create(user = User.objects.filter(username = user), page = Page.objects.filter(uniqueID = page))[0]
+        f.save()
+        return f
+
+    def add_friends(userA, userB):
+        f = Friends.objects.get_or_create(userA = User.objects.filter(username = userA), userB = User.objects.filter(username = userB))[0]
+        f.save()
+        return f
+    
+    def add_messages(sender, receiver, timeSent, content):
+        m = Message.objects.get_or_create(sender = User.objects.filter(username = sender), receiver = User.objects.filter(username = receiver), timeSent = timeSent)[0]
+        m.content = content
+        m.save()
+        return m
+    
+    def add_blog_post(user, page, timePosted, content):
+        b = BlogPost.objects.get_or_create(user = User.objects.filter(username = user), page = Page.objects.filter(uniqueID = page), timePosted = timePosted)[0]
+        b.content = content
+        b.save()
+        return b
+    
+    def add_featured(book, page):
+        f = Featured.objects.get_or_create(book = Book.objects.filter(isbn = book), page = Page.objects.filter(uniqueID = page))[0]
+        f.save()
+        return f
+    
+    def add_reading(user, book):
+        r = Reading.objects.get_or_create(user = User.objects.filter(username = user), book = Book.objects.filter(isbn = book))[0]
+        r.save()
+        return r
+    
+    def add_read(user, book, dateFinished):
+        r = Read.objects.get_or_create(user = User.objects.filter(username = user), book = Book.objects.filter(isbn = book), dateFinished = dateFinished)[0]
+        r.save()
+        return r
+    
+    def add_review(reviewer, book, rating, content):
+        r = Review.objects.get_or_create(reviewer = User.objects.filter(username = reviewer), book = Book.objects.filter(isbn = book))[0]
+        r.rating = rating
+        r.content = content
+        r.save()
+        return r
     
