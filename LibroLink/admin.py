fb018766f9f918e56179fef761bbd95b86b41563
Lucas Kennington <2726022k@student.gla.ac.uk>
2024-03-22 09:56:32 +0000

    Fix books template

--- LibroLink/admin.py
+++ LibroLink/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
 from LibroLink.models import UserProfile
+<<<<<<< Updated upstream
 
 # Register your models here.
 admin.site.register(UserProfile)
+=======
+from LibroLink.models import Category
+from LibroLink.models import Book
+from LibroLink.models import BookCategory
+from .models import Review
+
+
+# Register your models here.
+admin.site.register(UserProfile)
+admin.site.register(Review)
+admin.site.register(Book)
+admin.site.register(Category)
+admin.site.register(BookCategory)
+
+>>>>>>> Stashed changes
